9311ff922a5233505e016bbcc7409d74950727d1
petekozz <66498808+petekozz@users.noreply.github.com>
2020-10-28 20:24:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+
+const firstLink = document.querySelector('img');
+    firstLink.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+    firstLink.setAttribute('alt', 'chicken');
